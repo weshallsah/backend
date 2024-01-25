43a888a8a524957fae144acbdc7da199f7e54d36
vishal <vishalk74064@gmail.com>
2024-01-25 19:05:07 +0530

    mongodb error

---
+++ src/Db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+import { DB_Name } from "../constants.js";
+
+const connectDB =async ()=>{
+    try{
+        const Instance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
+        console.log(`\n mongoDB Connected`);
+    } catch(error){
+        console.error('Failed to Connect MongoDB',error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
